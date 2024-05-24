e0c7a43ceed622ed9a1fccc0b40e92b35987cbbb
Shaun Prince <suparious@solidrust.net>
2024-05-24 10:33:47 -0700

    define model for the message formatter

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ from llama_cpp_agent.chat_history import BasicChatHistory
 from llama_cpp_agent.chat_history.messages import Roles
 from llama_cpp_agent.llm_output_settings import LlmStructuredOutputSettings
 
-def respond(
-        message,
-        history: list[tuple[str, str]],
-        system_message,
-        max_tokens,
-        temperature,
-        top_p,
-        top_k,
-        repeat_penalty,
-        model,
-):
-    chat_template = get_messages_formatter_type(model)
 
+def respond(
+    message,
+    history: list[tuple[str, str]],
+    system_message,
+    max_tokens,
+    temperature,
+    top_p,
+    top_k,
+    repeat_penalty,
+    model,
+):
+    model = "Mistral"
     provider = TGIServerProvider("http://thanatos:8081")
+    
+    chat_template = get_messages_formatter_type(model)
 
     agent = LlamaCppAgent(
         provider,
         system_prompt=f"{system_message}",
         predefined_messages_formatter_type=chat_template,
-        debug_output=True
+        debug_output=True,
     )
 
     settings = provider.get_provider_default_settings()
@@ -38,33 +40,41 @@ def respond(
     settings.repeat_penalty = repeat_penalty
     settings.stream = True
     output_settings = LlmStructuredOutputSettings.from_functions(
-        [search_web, write_message_to_user])
+        [search_web, write_message_to_user]
+    )
     messages = BasicChatHistory()
 
     for msn in history:
-        user = {
-            'role': Roles.user,
-            'content': msn[0]
-        }
-        assistant = {
-            'role': Roles.assistant,
-            'content': msn[1]
-        }
+        user = {"role": Roles.user, "content": msn[0]}
+        assistant = {"role": Roles.assistant, "content": msn[1]}
         messages.add_message(user)
         messages.add_message(assistant)
-    result = agent.get_chat_response(message, llm_sampling_settings=settings, structured_output_settings=output_settings,
-                                     chat_history=messages,
-                                     print_output=False)
+    result = agent.get_chat_response(
+        message,
+        llm_sampling_settings=settings,
+        structured_output_settings=output_settings,
+        chat_history=messages,
+        print_output=False,
+    )
     while True:
         if result[0]["function"] == "write_message_to_user":
             break
         else:
-            result = agent.get_chat_response(result[0]["return_value"], role=Roles.tool, chat_history=messages,structured_output_settings=output_settings,
-                                             print_output=False)
+            result = agent.get_chat_response(
+                result[0]["return_value"],
+                role=Roles.tool,
+                chat_history=messages,
+                structured_output_settings=output_settings,
+                print_output=False,
+            )
 
     stream = agent.get_chat_response(
-        result[0]["return_value"], role=Roles.tool, llm_sampling_settings=settings, chat_history=messages, returns_streaming_generator=True,
-        print_output=False
+        result[0]["return_value"],
+        role=Roles.tool,
+        llm_sampling_settings=settings,
+        chat_history=messages,
+        returns_streaming_generator=True,
+        print_output=False,
     )
 
     outputs = ""
@@ -76,7 +86,10 @@ def respond(
 main = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value="You are an advanced AI agent for summarizing and answer search engine result.", label="System message"),
+        gr.Textbox(
+            value="You are an advanced AI agent for summarizing and answer search engine result.",
+            label="System message",
+        ),
         gr.Slider(minimum=1, maximum=4096, value=2048, step=1, label="Max tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
@@ -105,25 +118,26 @@ main = gr.ChatInterface(
         primary_hue="violet",
         secondary_hue="violet",
         neutral_hue="gray",
-        font=[gr.themes.GoogleFont("Exo"), "ui-sans-serif", "system-ui", "sans-serif"]).set(
-            body_background_fill_dark="#111111",
-            block_background_fill_dark="#111111",
-            block_border_width="1px",
-            block_title_background_fill_dark="#1e1c26",
-            input_background_fill_dark="#292733",
-            button_secondary_background_fill_dark="#24212b",
-            border_color_primary_dark="#343140",
-            background_fill_secondary_dark="#111111",
-            color_accent_soft_dark="transparent"
-        ),
-        css=css,
-        retry_btn="Retry",
-        undo_btn="Undo",
-        clear_btn="Clear",
-        submit_btn="Send",
-        description="Llama-cpp-agent: Chat Web Search DDG Agent",
-        chatbot=gr.Chatbot(scale=1, placeholder=PLACEHOLDER)
-    )
+        font=[gr.themes.GoogleFont("Exo"), "ui-sans-serif", "system-ui", "sans-serif"],
+    ).set(
+        body_background_fill_dark="#111111",
+        block_background_fill_dark="#111111",
+        block_border_width="1px",
+        block_title_background_fill_dark="#1e1c26",
+        input_background_fill_dark="#292733",
+        button_secondary_background_fill_dark="#24212b",
+        border_color_primary_dark="#343140",
+        background_fill_secondary_dark="#111111",
+        color_accent_soft_dark="transparent",
+    ),
+    css=css,
+    retry_btn="Retry",
+    undo_btn="Undo",
+    clear_btn="Clear",
+    submit_btn="Send",
+    description="Llama-cpp-agent: Chat Web Search DDG Agent",
+    chatbot=gr.Chatbot(scale=1, placeholder=PLACEHOLDER),
+)
 
 if __name__ == "__main__":
     main.launch()

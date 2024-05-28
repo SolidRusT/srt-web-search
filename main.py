dfa8db7f6d2d871402466489a7fbf91c4282f538
Shaun Prince <suparious@solidrust.net>
2024-05-27 21:02:18 -0700

    clean-up cpp-agent positional arguments

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ persona_preferences = config.preferences
 # provider = LlamaCppServerProvider(llm_url)
 
 ## vLLM Server provider
-llm_url = "http://thanatos.hq.solidrust.net:8082/v1"
-#llm_url = "http://thanatos:8081/v1"
+#llm_url = "http://thanatos.hq.solidrust.net:8082/v1"
+llm_url = "http://thanatos:8081/v1"
 model = "solidrust/Mistral-7B-instruct-v0.3-AWQ"
 llm_model_type = "Mistral"          # config.current_settings[0]["model_type"]
 llm_max_tokens = 16384 
@@ -68,8 +68,8 @@ provider = VLLMServerProvider(
     huggingface_model=model,
 )
 ## vLLM Server provider for summary
-llm_summary_url = "http://zelus.hq.solidrust.net:8083/v1"
-#llm_summary_url = "http://zelus:8081/v1"
+#llm_summary_url = "http://zelus.hq.solidrust.net:8083/v1"
+llm_summary_url = "http://zelus:8081/v1"
 model_summary = "solidrust/Mistral-7B-instruct-v0.3-AWQ"
 llm_model_summary_type = "Llama3"
 llm_summary_max_tokens = 8182
@@ -130,7 +130,7 @@ def respond(
 
     logging.info(f"Loaded chat examples: {chat_template}")
     search_tool = WebSearchTool(
-        llm_provider=model,
+        llm_provider=provider,
         message_formatter_type=chat_template,
         model_max_context_tokens=llm_max_tokens,
         max_tokens_search_results=tokens_search_results,
@@ -139,14 +139,14 @@ def respond(
     )
 
     web_search_agent = LlamaCppAgent(
-        provider,
+        provider=provider,
         system_prompt=web_search_system_prompt,
         predefined_messages_formatter_type=chat_template,
         debug_output=False,
     )
 
     answer_agent = LlamaCppAgent(
-        provider,
+        provider=provider,
         system_prompt=system_message,
         predefined_messages_formatter_type=chat_template,
         debug_output=True,

622dd59810a4f122d08cd62592b358df90028ed4
Shaun Prince <suparious@solidrust.net>
2024-05-30 05:47:09 -0700

    simplify gradio interface.

--- gradio_interface.py
+++ gradio_interface.py
@@ -12,40 +12,26 @@ class GradioInterface:
     def setup(self):
         try:
             logging.info("Setting up Gradio interface components.")
-            additional_inputs = [
-                gr.Textbox(value=self.system_message, label="System message", interactive=True),
-                gr.Textbox(label="Your message", interactive=True),
-                gr.Slider(minimum=1, maximum=4096, value=2048, step=1, label="Max tokens"),
-                gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-                gr.Slider(minimum=0.1, maximum=1.0, value=0.95, step=0.05, label="Top-p"),
-                gr.Slider(minimum=0, maximum=100, value=40, step=1, label="Top-k"),
-                gr.Slider(
-                    minimum=0.0,
-                    maximum=2.0,
-                    value=1.1,
-                    step=0.1,
-                    label="Repetition penalty",
-                ),
+            inputs = [
+                gr.Textbox(value=self.system_message, label="System message", interactive=True, visible=False),
+                gr.Textbox(label="Your message", interactive=True)
             ]
             if self.is_wikipedia:
-                additional_inputs.insert(1, gr.Textbox(label="Wikipedia Page Title", interactive=True))
+                inputs.insert(1, gr.Textbox(label="Wikipedia Page Title", interactive=True))
 
-            async def response_fn_wrapper(*inputs):
+            async def response_fn_wrapper(system_message, page_title, message):
                 try:
                     params = {
-                        "system_message": inputs[0],
-                        "message": inputs[2] if self.is_wikipedia else inputs[1],
-                        "max_tokens": inputs[-5],
-                        "temperature": inputs[-4],
-                        "top_p": inputs[-3],
-                        "top_k": inputs[-2],
-                        "repetition_penalty": inputs[-1],
+                        "system_message": system_message,
+                        "message": message,
+                        "max_tokens": 2048,
+                        "temperature": 0.7,
+                        "top_p": 0.95,
+                        "top_k": 40,
+                        "repetition_penalty": 1.1,
                         "model": config.default_llm_huggingface,
+                        "page_title": page_title if self.is_wikipedia else None,
                     }
-                    if self.is_wikipedia:
-                        params["page_title"] = inputs[1]
-                    else:
-                        params["page_title"] = None
                     
                     response_gen = self.response_function(**params)
                     response_text = ""
@@ -56,10 +42,11 @@ class GradioInterface:
                     logging.error(f"Error occurred during response generation: {e}", exc_info=True)
                     yield "An error occurred while processing your request. Please try again later."
 
-            logging.info("Creating Gradio ChatInterface.")
-            return gr.ChatInterface(
-                response_fn_wrapper,
-                additional_inputs=additional_inputs,
+            logging.info("Creating Gradio Interface.")
+            return gr.Interface(
+                fn=response_fn_wrapper,
+                inputs=inputs,
+                outputs=gr.Textbox(),
                 theme=gr.themes.Soft(
                     primary_hue="orange",
                     secondary_hue="amber",
@@ -84,16 +71,9 @@ class GradioInterface:
                     code_background_fill_dark="#140b0b",
                 ),
                 css=css,
-                retry_btn="Retry",
-                undo_btn="Undo",
-                clear_btn="Clear",
-                submit_btn="Send",
                 examples=config.persona_topic_examples,
                 analytics_enabled=False,
                 description="Llama-cpp-agent Interface",
-                chatbot=gr.Chatbot(
-                    scale=1, placeholder=PLACEHOLDER, likeable=False, show_copy_button=True
-                ),
             )
         except Exception as e:
             logging.error(f"Error during Gradio setup: {e}", exc_info=True)

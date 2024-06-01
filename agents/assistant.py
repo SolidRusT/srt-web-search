60fcac105f8cdc10ee428b9388c30c203b11edf8
Shaun Prince <suparious@solidrust.net>
2024-06-01 07:03:12 -0700

    enable the config system

--- agents/assistant.py
+++ agents/assistant.py
@@ -1,12 +1,14 @@
 from llama_cpp_agent.messages_formatter import MessagesFormatterType
-from llama_cpp_agent.providers import LlamaCppServerProvider
+from app.config import config
+from app.messages import MessageHandler
 from agents.memgpt import MemGptAgent
 
-provider = LlamaCppServerProvider(server_address="http://127.0.0.1:8080")
+provider = config.chat_provider
+chat_template = MessageHandler.get_messages_formatter_type(config.chat_llm_type)
 
 mem_gpt_agent = MemGptAgent(provider, debug_output=True, core_memory_file="../data/core_memory.json",
-                            event_queue_file="event_queue.json",
-                            messages_formatter_type=MessagesFormatterType.CHATML
+                            event_queue_file="../data/event_queue.json",
+                            messages_formatter_type=chat_template
                             )
 
 while True:

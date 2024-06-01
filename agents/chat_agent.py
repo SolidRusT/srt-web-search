2139e503670854ea298392c33c2bc07dd550ef48
Shaun Prince <suparious@solidrust.net>
2024-06-01 06:35:42 -0700

    update chat agent to use the chat provider

--- agents/chat_agent.py
+++ agents/chat_agent.py
@@ -10,18 +10,19 @@ from llama_cpp_agent.providers.llama_cpp_python import LlamaCppPythonSamplingSet
 
 async def chat_response(system_message, message, history, max_tokens, temperature, top_p, top_k, repetition_penalty, model):
     system_message = f"{config.persona_system_message} {config.persona_prompt_message}"
-    chat_template = MessageHandler.get_messages_formatter_type(config.default_llm_type)
+    chat_template = MessageHandler.get_messages_formatter_type(config.chat_llm_type)
 
     chat_agent = LlamaCppAgent(
-        provider=config.default_provider,
+        provider=config.chat_provider,
         system_prompt=system_message,
         predefined_messages_formatter_type=chat_template,
         debug_output=config.debug,
     )
-    default_agent_provider = config.default_llm_agent_provider
+    #default_agent_provider = config.default_llm_agent_provider
+    default_agent_provider = config.chat_llm_agent_provider
     logging.info(f"Loaded chat template: {chat_template}")
     
-    settings = config.default_provider.get_provider_default_settings()
+    settings = config.chat_provider.get_provider_default_settings()
     settings.stream = True
     settings.temperature = temperature
     settings.top_k = top_k
